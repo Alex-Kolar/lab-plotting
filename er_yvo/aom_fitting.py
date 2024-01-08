56210aaea11b85097e95fbd6443fe81e7ac625f9
Alex-Kolar <alexkolar@comcast.net>
2024-01-08 15:13:18 -0600

    Add combined plotting to aom_fitting.py; Add optical gain

--- er_yvo/aom_fitting.py
+++ er_yvo/aom_fitting.py
@@ -2,6 +2,7 @@ import glob
 import os
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from lmfit.models import GaussianModel, ConstantModel
 
@@ -11,6 +12,11 @@ DATA_DIR = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab"
 OUTPUT_DIR = "/Users/alexkolar/Desktop/Lab/lab-plotting/output_figs/aom_fitting/112ns_offset"
 FILES_IGNORE = ["30ns/NewFile1.csv",
                 "30ns/NewFile2.csv"]
+GAIN = 700  # units: V/W
+
+# plotting parameters
+mpl.rcParams.update({'font.size': 12,
+                     'figure.figsize': (8, 6)})
 
 
 # get all dataframes
@@ -32,6 +38,10 @@ for i, path in enumerate(paths):
     time_array = np.linspace(0, (num_points-1)*increment, num_points)
     time_arrays.append(time_array)
 
+# convert optical units
+for df in dfs:
+    df["CH3"] = df["CH3"].apply(lambda x: 1e3*(x/GAIN))  # convert to mW
+
 
 # do fitting
 hdawg_fits = []
@@ -50,15 +60,19 @@ for i, df in enumerate(dfs):
     aom_fits.append(aom_fit)
 
 
+# plot individual widths
 for i, df in enumerate(dfs):
     num_points = len(df["CH1"])
     time_array = time_arrays[i]
 
-    plt.plot(time_array, df["CH1"], label="HDAWG Output")
-    plt.plot(time_array, df["CH3"], label="AOM Output")
-    plt.plot(time_array, hdawg_fits[i].best_fit,
-             'k--', label="HDAWG Fit")
-    plt.plot(time_array, aom_fits[i].best_fit,
+    fig, ax = plt.subplots()
+    ax1 = ax.twinx()
+
+    ax.plot(time_array, df["CH1"], label="HDAWG Output", color='tab:blue')
+    ax1.plot(time_array, df["CH3"], label="AOM Output", color='tab:orange')
+    ax.plot(time_array, hdawg_fits[i].best_fit,
+            'k--', label="HDAWG Fit")
+    ax1.plot(time_array, aom_fits[i].best_fit,
              'k:', label="AOM Fit")
 
     hdawg_lw = hdawg_fits[i].params['fwhm'].value
@@ -68,14 +82,92 @@ for i, df in enumerate(dfs):
     res_str = rf"HDAWG FWHM: {hdawg_lw:0.3} $\pm$ {hdawg_err:0.3} ns"
     res_str += "\n"
     res_str += rf"AOM FWHM: {aom_lw:0.3} $\pm$ {aom_err:0.3} ns"
-    plt.text(300, -0.75, res_str)
+    ax.text(200, 0, res_str)
 
-    plt.title(f"{desired_pulse_width[i]} pulse width")
-    plt.xlabel("Time (ns)")
+    ax.set_title(f"{desired_pulse_width[i]} pulse width")
+    ax.set_xlabel("Time (ns)")
+    ax.set_ylabel("HDAWG Voltage (V)")
+    ax1.set_ylabel("Optical Output Power (mW)")
 
     plt.grid(True)
     plt.legend()
 
+    plt.tight_layout()
     output_filename = os.path.join(OUTPUT_DIR, f"{desired_pulse_width[i]}.png")
     plt.savefig(output_filename)
     plt.clf()
+
+# plot of size versus width
+def get_area(x):
+    return x.params['amplitude'].value
+
+def get_area_err(x):
+    return x.params['amplitude'].stderr
+
+def get_height(x):
+    return x.params['height'].value
+
+def get_height_err(x):
+    return x.params['height'].stderr
+
+def get_lw(x):
+    return x.params['fwhm'].value
+
+def get_lw_err(x):
+    return x.params['fwhm'].stderr
+
+
+widths = list(map(get_lw, aom_fits))
+# width_err = list(map(get_lw_err, aom_fits))
+heights = list(map(get_height, aom_fits))
+# height_err = list(map(get_height_err, aom_fits))
+areas = list(map(get_area, aom_fits))
+
+
+# plot both on same graph
+fig, ax = plt.subplots()
+ax1 = ax.twinx()
+
+color_height = 'tab:blue'
+color_area = 'tab:orange'
+
+# ax.errorbar(widths, heights, xerr=width_err, yerr=height_err,
+#             ls='', marker='')
+ax.loglog(widths, heights, 'o', color=color_height)
+ax1.loglog(widths, areas, 'o', color=color_area)
+
+ax.set_xlabel("Optical FWHM (ns)")
+ax.set_ylabel("Height (mW)", color=color_height)
+ax1.set_ylabel("Area (mW * ns)", color=color_area)
+ax.tick_params(axis='y', colors=color_height)
+ax1.tick_params(axis='y', colors=color_area)
+ax.grid(True)
+
+fig.tight_layout()
+fig.show()
+
+
+# plot only heights
+fig, ax = plt.subplots()
+
+ax.plot(widths, heights, 'o', color='tab:blue')
+
+ax.set_xlabel("Optical FWHM (ns)")
+ax.set_ylabel("Height (mW)")
+ax.grid(True)
+
+fig.tight_layout()
+fig.show()
+
+
+# plot only areas
+fig, ax = plt.subplots()
+
+ax.plot(widths, areas, 'o', color='tab:orange')
+
+ax.set_xlabel("Optical FWHM (ns)")
+ax.set_ylabel("Area (mW * ns)")
+ax.grid(True)
+
+fig.tight_layout()
+fig.show()
