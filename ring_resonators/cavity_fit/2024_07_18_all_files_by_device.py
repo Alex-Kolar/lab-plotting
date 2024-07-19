6cf23cada70692012b00f49a12b32e245021b743
Alex-Kolar <alexkolar@comcast.net>
2024-07-19 10:36:42 -0500

    Add initial version of fitting script for cavity resonances July 2024

---
+++ ring_resonators/cavity_fit/2024_07_18_all_files_by_device.py
@@ -0,0 +1,187 @@
+import glob
+import os
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from lmfit.models import BreitWignerModel, ConstantModel
+from scipy.signal import find_peaks
+
+
+DATA_DIR = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators"
+            "/New_mounted_device/07182024")
+CSV_PATH = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators"
+            "/New_mounted_device/07182024/resonances_07_18_2024.csv")
+OUTPUT_DIR = ("/Users/alexkolar/Desktop/Lab/lab-plotting/output_figs/ring_resonators"
+              "/new_mounted/room_temp_07182024/testing")
+
+# fitting params
+SMOOTHING = 21
+PEAK_THRESH = 0.85
+
+# plotting params
+mpl.rcParams.update({'font.sans-serif': 'Helvetica',
+                     'font.size': 12})
+color = 'cornflowerblue'
+
+PLOT_ALL_RES = True  # plot and save all intermediate results
+
+
+# moving average function for peaks
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
+# read csv
+main_df = pd.read_csv(CSV_PATH)
+
+# find and read oscilloscope files
+filenames = glob.glob('SDS*.csv', root_dir=DATA_DIR)
+data_dfs = {}
+for file in filenames:
+    file_str = os.path.splitext(file)[0]
+    file_num = int(file_str[3:])
+    file_path = os.path.join(DATA_DIR, file)
+    data_dfs[file_num] = pd.read_csv(file_path, header=10, skiprows=[11])
+
+
+# main data processing loop
+print('Fitting all resonances.')
+num_rows = len(main_df['FileNumber'])
+# save specific params for plotting combined results
+center_by_device = {}
+q_by_device = {}
+
+for _, row in main_df.iterrows():
+    # get info
+    device = int(row['Device'])
+    freq_start = row['Min']  # unit: GHz
+    freq_end = row['Max']  # unit: GHz
+    file_no = int(row['FileNumber'])
+
+    print(f'\tFitting row {file_no}/{num_rows}')
+    print(f'\t\tDevice: {device}')
+    print(f'\t\tFreq start: {freq_start}')
+
+    # get data associated with row
+    data_df = data_dfs[file_no]
+    ramp = data_df['CH1'].astype(float)
+    transmission = data_df['CH2'].astype(float)
+
+    id_min = np.argmin(ramp)
+    id_max = np.argmax(ramp)
+    transmission = transmission[id_min:id_max]
+    transmission.reset_index(drop=True, inplace=True)
+    freq = np.linspace(0, (freq_end-freq_start)*1e3,
+                       num=(id_max-id_min))  # unit: MHz
+
+    # find peaks to determine number of fits
+    smoothed_data = moving_average(transmission.to_numpy(),
+                                   n=SMOOTHING)
+    peaks, peak_res = find_peaks(-smoothed_data,
+                                 prominence=0.015, distance=50, width=10)
+    peaks_to_keep = [p for p in peaks
+                     if transmission[p] < (max(transmission)*PEAK_THRESH)]
+    print(f"\t\tnumber of peaks: {len(peaks_to_keep)}")
+
+    # do fitting
+    model = ConstantModel()
+    model_kwargs = {}
+    for i, peak_idx in enumerate(peaks_to_keep):
+        model += BreitWignerModel(prefix=f'p{i}_')
+        model_kwargs[f'p{i}_center'] = freq[peak_idx]
+        model_kwargs[f'p{i}_sigma'] = 500
+
+    center_guess = freq[np.argmin(transmission)]
+    sigma_guess = 500
+    res = model.fit(transmission, x=freq,
+                    **model_kwargs)
+
+    wl = 3e8 / (freq_start * 1e9)  # unit: m
+    wl *= 1e9  # unit: nm
+
+    plt.plot(freq, transmission, color=color,
+             label='Data')
+    plt.plot(freq, res.init_fit, 'r--',
+             label='Initial Fit')
+    plt.plot(freq, res.best_fit, 'k--',
+             label='Fit')
+
+    # # peaks for testing
+    # plt.plot(freq[peaks_to_keep], transmission[peaks_to_keep],
+    #          'x', color='k')
+
+    # extract data
+    all_center_curr = []
+    all_q_curr = []
+    for i in range(len(peaks_to_keep)):
+        width = res.params[f'p{i}_sigma'].value  # unit: MHz
+        center = res.params[f'p{i}_center'].value  # unit: MHz
+        freq_light = (freq_start * 1e3) + center  # unit: MHz
+        q = freq_light / width
+        all_center_curr.append(freq_light)
+        all_q_curr.append(q)
+
+        text = rf"$\Gamma$: {width:.3f} MHz"
+        text += "\n"
+        text += f"Q: {q:.3}"
+        plt.text(center + 500,
+                 transmission[peaks_to_keep[i]] - 0.1,
+                 text)
+
+    if device in center_by_device:
+        center_by_device[device] += all_center_curr
+        q_by_device[device] += all_q_curr
+    else:
+        center_by_device[device] = all_center_curr
+        q_by_device[device] = all_q_curr
+
+
+    plt.title(f"Device {device} {wl:.3f} nm ({freq_start * 1e-3:.3f} THz) scan")
+    plt.legend(shadow=True)
+    plt.xlabel("Detuning (MHz)")
+    plt.ylabel("Transmission (A.U.)")
+    plt.grid(True)
+
+    plt.tight_layout()
+
+    save_name = f"D{device}_{wl:.3f}"
+    save_name = save_name.replace(".", "_")
+    save_name += ".png"
+    output_path = os.path.join(OUTPUT_DIR, str(device))
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    plt.savefig(os.path.join(output_path, save_name))
+    plt.clf()
+
+
+# make bar graph of highest q
+bar_x = range(len(q_by_device.keys()))
+labels = list(q_by_device.keys())
+bar_y = [max(vals) for vals in q_by_device.values()]
+plt.bar(bar_x, bar_y,
+        color=color, edgecolor='k', zorder=2)
+
+plt.xticks(bar_x, labels)
+plt.xlabel("Device Number")
+plt.ylabel("Highest Q Factor")
+plt.grid(axis='y')
+
+plt.tight_layout()
+plt.show()
+
+
+# make plot of q versus freq for each device
+for device in center_by_device:
+    centers = np.array(center_by_device[device])
+    centers *= 1e-6  # convert to THz
+    plt.stem(centers, q_by_device[device],
+             linefmt=color, markerfmt=color, basefmt='k')
+    plt.title(f"Device {device}")
+    plt.xlabel("Frequency (THz)")
+    plt.ylabel("Q Factor")
+
+    plt.tight_layout()
+    plt.show()
