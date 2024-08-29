61a43705fb0ecca0a5cc133d6683465d448f2372
Alex-Kolar <alexkolar@comcast.net>
2024-08-29 09:33:17 -0500

    EOM code

---
+++ ring_resonators/eom/compare_methods.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+PHASE_PATH = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators"
+              "/eom_data/08082024/100 no atten.csv")
+AMP_PATH = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators"
+            "/eom_data/08092024/100.csv")
+
+# plotting params
+mpl.rcParams.update({'font.sans-serif': 'Helvetica',
+                     'font.size': 12})
+
+
+# find and read files
+phase_data = pd.read_csv(PHASE_PATH, header=1)
+amp_data = pd.read_csv(AMP_PATH, header=1)
+
+
+plt.plot(phase_data['Freq'] / 1e6, phase_data['Amp'],
+         label='Phase Modulation')
+plt.plot(amp_data['Freq'] / 1e6, amp_data['Amp'],
+         label='Amplitude Modulation')
+
+plt.title(f"Comparing Methods")
+plt.xlabel("Frequency (MHz)")
+plt.ylabel("Power (dBm)")
+plt.legend(shadow=True)
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
