7a92e7ce9b8517a9b103e250df82e939fa267870
Alex-Kolar <alexkolar@comcast.net>
2024-10-30 17:11:07 -0500

    Add fitting for cavity data using SNSPD

---
+++ ring_resonators/cavity_fit/2024_10_09_snspd.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from lmfit.models import LorentzianModel, ConstantModel
+
+
+INIT_DATA = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators"
+             "/New_mounted_device/10mK/snspd_10092024/no_pump_Histogram_2024-10-09_16-38-22.txt")
+PUMP_DATA = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators"
+             "/New_mounted_device/10mK/snspd_10092024/pump_Histogram_2024-10-09_17-21-05.txt")
+
+# plotting params
+mpl.rcParams.update({'font.sans-serif': 'Arial',
+                     'font.size': 12})
+xlim = (4e9, 6e9)
+
+
+df_init = pd.read_csv(INIT_DATA, sep='\t')
+df_pump = pd.read_csv(PUMP_DATA, sep='\t')
+
+
+# plotting
+plt.plot(df_init['time(ps)'], df_init['counts'],
+         color='cornflowerblue')
+plt.title("Before Pumping")
+plt.xlabel("Tagger Time (ps)")
+plt.ylabel("Counts")
+plt.xlim(xlim)
+
+plt.tight_layout()
+plt.show()
+
+plt.plot(df_pump['time(ps)'], df_pump['counts'],
+         color='cornflowerblue')
+plt.title("After Pumping")
+plt.xlabel("Tagger Time (ps)")
+plt.ylabel("Counts")
+plt.xlim(xlim)
+
+plt.tight_layout()
+plt.show()
+
+plt.plot(df_init['time(ps)'], df_pump['counts']/df_init['counts'],
+         color='coral')
+plt.title("Ratio After/Before Pumping")
+plt.xlabel("Tagger Time (ps)")
+plt.xlim(xlim)
+
+plt.tight_layout()
+plt.show()
---
+++ ring_resonators/cavity_fit/2024_10_10_snspd.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from lmfit.models import LorentzianModel, ConstantModel
+
+
+INIT_DATA = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators"
+             "/New_mounted_device/10mK/snspd_10102024/no_pump_Histogram_2024-10-10_14-54-19.txt")
+PUMP_DATA = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators"
+             "/New_mounted_device/10mK/snspd_10102024/pump_Histogram_2024-10-10_15-26-05.txt")
+
+# plotting params
+mpl.rcParams.update({'font.sans-serif': 'Arial',
+                     'font.size': 12})
+xlim = (4.75e9, 5.75e9)
+
+
+df_init = pd.read_csv(INIT_DATA, sep='\t')
+df_pump = pd.read_csv(PUMP_DATA, sep='\t')
+
+
+# plotting
+plt.plot(df_init['time(ps)'], df_init['counts'],
+         color='cornflowerblue')
+plt.title("Before Pumping")
+plt.xlabel("Tagger Time (ps)")
+plt.ylabel("Counts")
+plt.xlim(xlim)
+
+plt.tight_layout()
+plt.show()
+
+plt.plot(df_pump['time(ps)'], df_pump['counts'],
+         color='cornflowerblue')
+plt.title("After Pumping")
+plt.xlabel("Tagger Time (ps)")
+plt.ylabel("Counts")
+plt.xlim(xlim)
+
+plt.tight_layout()
+plt.show()
+
+plt.plot(df_init['time(ps)'], df_pump['counts']/df_init['counts'],
+         color='coral')
+plt.title("Ratio After/Before Pumping")
+plt.xlabel("Tagger Time (ps)")
+plt.xlim(xlim)
+
+plt.tight_layout()
+plt.show()
