9b4b0fc4e31e5152aa7cbec34bdc64f70492bcfe
Alex-Kolar <alexkolar@comcast.net>
2024-10-30 17:12:07 -0500

    Modify fano fitting format

--- ring_resonators/cavity_fit/2023_08_02_fano_fitting.py
+++ ring_resonators/cavity_fit/2023_08_02_fano_fitting.py
@@ -9,11 +9,14 @@ DATA = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Re
         "/Original_device/Coincidence Count Measurement/08022023/SDS00002.csv")
 WAVELENGTH = 1537.782  # unit: nm
 SCAN_RANGE = 2.7  # unit: GHz
+SAVE_FIG = True
+SAVENAME = ("/Users/alexkolar/Desktop/Lab/lab-plotting/output_figs"
+            "/ring_resonators/original/coincidence_resonance_fit.svg")
 
 # plotting params
 mpl.rcParams.update({'font.sans-serif': 'Helvetica',
                      'font.size': 12})
-xlim = (1000, 2000)
+xlim = (-500, 500)
 color = 'cornflowerblue'
 
 # fitting params
@@ -50,26 +53,27 @@ print(out.fit_report())
 # print out relevant information
 sigma = out.params['sigma'].value  # unit: MHz
 sigma_err = out.params['sigma'].stderr
-# print("Sigma:", sigma, "MHz")
 print(f"Sigma: {sigma:0.3f} +/- {sigma_err:0.3f} MHz")
 freq_light = (3e8 / (WAVELENGTH * 1e-9)) * 1e-6  # unit: MHz
 print("Cavity freq:", freq_light, "MHz")
 q = freq_light / sigma
 print("Q:", q)
 
+# center
+freq = freq - out.params['center'].value
+
 
 # plotting
 fig, ax = plt.subplots()
 
-ax.plot(freq[idx_to_fit], transmission[idx_to_fit], color=color, label="Transmission")
+ax.plot(freq[idx_to_fit], transmission[idx_to_fit], color=color, label="Data")
 ax.plot(freq[idx_to_fit], out.best_fit, '--k', label="Fit")
-# ax.plot(freq[idx_to_fit], out.init_fit, '--r', label="Initial")
 ax.set_xlim(xlim)
 # ax.grid(True)
 ax.legend(shadow=True)
 ax.set_title(f"Cavity Resonance at {WAVELENGTH} nm")
-ax.set_xlabel("Frequency (MHz)")
-ax.set_ylabel("Transmission (A.U.)")
+ax.set_xlabel("Detuning (MHz)")
+ax.set_ylabel("Cavity Reflection (A.U.)")
 
 # add relevant info
 label = r"$\Gamma$: {:0.3f} $\pm$ {:0.3f} MHz".format(
@@ -79,7 +83,10 @@ t = ax.text(0.95, 0.05, label,
 t.set_transform(ax.transAxes)
 
 fig.tight_layout()
-fig.show()
+if SAVE_FIG:
+    fig.savefig(SAVENAME)
+else:
+    fig.show()
 
 # # for Milan measurements
 # time_length = np.max(time) - np.min(time)
