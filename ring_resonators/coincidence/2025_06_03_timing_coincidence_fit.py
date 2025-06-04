a1c1ebf7007e45f26d8b6b78938d6a15766ad453
Alex-Kolar <alexkolar@comcast.net>
2025-06-04 11:25:38 -0500

    Add fitting of coincidence for June 3 data

---
+++ ring_resonators/coincidence/2025_06_03_timing_coincidence_fit.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from lmfit import Model, Parameters
+
+from ring_resonators.cavity_fit.cavity_metrics import g_2_no_delta
+
+
+FILENAME = ("/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators"
+            "/New_mounted_device/4K/06032025/Correlation_2025-06-03_17-47-42.txt")
+FITTING = True  # add a fit
+WAVELENGTH = 1537.502  # units: nm
+
+
+# plotting params
+mpl.rcParams.update({'font.sans-serif': 'Helvetica',
+                     'font.size': 12})
+xlim_range = 15  # size of x lims
+fit_range = 100  # size for fitting
+color = 'coral'
+
+
+# NOTE: first line of csv needs extra tab added
+# otherwise the columns will not be read properly
+df = pd.read_csv(FILENAME, sep='\t')
+coincidence = df["Counts"]
+time = df["Time(ps)"]  # unit: ps
+time *= 1e-3  # unit: ns
+time_diff = time[1] - time[0]  # spacing of histogram
+
+
+# fitting
+if FITTING:
+    model = Model(g_2_no_delta)
+
+    x0_guess = 53
+    amplitude_guess = 5
+    kappa_guess = 1
+    g_guess = 0.125
+
+    # get fit range
+    idx_to_fit = np.where(np.abs(time - x0_guess) < (fit_range/2))[0]
+    time_to_fit = time[idx_to_fit]
+    coincidence_to_fit = coincidence[idx_to_fit]
+
+    params = Parameters()
+    params.add('x0', value=x0_guess)
+    params.add('amplitude', value=amplitude_guess)
+    params.add('kappa', value=kappa_guess)
+    params.add('g_factor', value=g_guess/kappa_guess, max=0.5)
+    params.add('g', expr='g_factor * kappa')
+    res = model.fit(coincidence_to_fit, params, x=time_to_fit)
+    print(res.fit_report())
+
+    # res = model.fit(coincidence, x=time,
+    #                 x0=x0_guess,
+    #                 amplitude=amplitude_guess,
+    #                 kappa=kappa_guess,
+    #                 g=g_guess)
+    # print(res.fit_report())
+
+
+# plotting
+fig, ax = plt.subplots()
+
+ax.bar(time, coincidence, width=time_diff, color=color, label='Data')
+
+if FITTING:
+    ax.plot(time_to_fit, res.best_fit, 'k--', label='Fit')
+
+    # extract relevant info
+    kappa = res.params['kappa'].value  # unit: 2*pi*GHz
+    kappa /= 2*np.pi
+    kappa *= 1e3  # unit: MHz
+    kappa_err = res.params['kappa'].stderr  # unit: GHz
+    kappa_err /= 2*np.pi
+    kappa_err *= 1e3  # unit: MHz
+
+    label = r"$\kappa$: {:0.3f} $\pm$ {:0.3f} MHz".format(
+        kappa, kappa_err)
+    t = ax.text(0.05, 0.95, label,
+                horizontalalignment='left', verticalalignment='top')
+    t.set_transform(ax.transAxes)
+
+if FITTING:
+    center = res.params['x0'].value
+else:
+    center = time[np.argmax(coincidence)]
+xlim = (center - xlim_range/2, center + xlim_range/2)
+ax.set_xlim(xlim)
+ax.set_xlabel("Timing Offset (ns)")
+ax.set_ylabel("Coincidence Counts")
+ax.set_title("Two-Photon Coincidence")
+plt.legend(shadow=True)
+
+fig.tight_layout()
+fig.show()
