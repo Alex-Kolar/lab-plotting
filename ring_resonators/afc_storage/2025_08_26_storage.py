1f90466e6c148543b34a68edace36d155630fbef
Alex-Kolar <alexkolar@comcast.net>
2025-08-27 19:50:39 -0500

    Add AFC storage from August 26 and 27 (test magnetic field and initialization)

---
+++ ring_resonators/afc_storage/2025_08_26_storage.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+
+
+OFFRES_DATA = ('/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators'
+               '/Mounted_device_mk_4/10mK/2025_08_26/afc/afc_storage_experiment_325mT_offres.npz')
+STORAGE_DATA = ('/Users/alexkolar/Library/CloudStorage/Box-Box/Zhonglab/Lab data/Ring Resonators'
+                '/Mounted_device_mk_4/10mK/2025_08_26/afc/afc_storage_experiment_350mT.npz')
+
+
+# plotting params
+mpl.rcParams.update({'font.sans-serif': 'Helvetica',
+                     'font.size': 12})
+color_offres = 'cornflowerblue'
+color = 'coral'
+PLOT_PEAKS = False
+xlim = (0.9, 1.15)
+
+
+# read data
+data_offres = np.load(OFFRES_DATA, allow_pickle=True)
+data_echo = np.load(STORAGE_DATA, allow_pickle=True)
+
+
+# plotting overlaid
+time = data_offres['bins']
+time_diff = time[1] - time[0]
+plt.bar(time, data_offres['counts'],
+        width=time_diff,
+        color=color_offres, alpha=0.5,
+        label='Off-Resonant Pulse')
+plt.bar(time, data_echo['counts'],
+        width=time_diff,
+        color=color, alpha=0.5,
+        label='Echo Pulse')
+
+plt.title('Echo Measurement')
+plt.xlabel(r'Time ($\mathrm{\mu}$s)')
+plt.ylabel('Counts')
+plt.legend(shadow=True)
+plt.yscale('log')
+plt.xlim(xlim)
+
+plt.tight_layout()
+plt.show()
+
+
+# # plotting overlaid (better)
+# idx_to_plot = np.where(np.logical_and(time >= xlim[0], time <= xlim[1]))[0]
+# time_to_plot = time[idx_to_plot]
+# offres_to_plot = data_offres['counts'][idx_to_plot]
+# echo_to_plot = data_echo['counts'][idx_to_plot]
+#
+# fig, ax = plt.subplots(figsize=(4.5, 4), dpi=400)
+#
+# ax.bar(time_to_plot, offres_to_plot,
+#        width=time_diff,
+#        color=color_offres, alpha=0.5,
+#        label='Off-Resonant Pulse')
+# ax.bar(time_to_plot, echo_to_plot,
+#        width=time_diff,
+#        color=color, alpha=0.5,
+#        label='Echo Pulse')
+#
+# ax.set_title('Echo Measurement')
+# ax.set_xlabel(r'Time ($\mathrm{\mu}$s)')
+# ax.set_ylabel('Counts')
+# ax.legend(frameon=False)
+# ax.set_yscale('log')
+# ax.set_xlim(xlim)
+#
+# fig.tight_layout()
+# fig.show()
