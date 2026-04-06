edcd3ebb05176f39cca331849f5d9a300523fd91
Alex-Kolar <alexkolar@comcast.net>
2026-04-06 10:15:58 -0500

    add error bars to interference sweep

--- ring_resonators/afc_storage_interference/2026_02_25_two_interference_sweep.py
+++ ring_resonators/afc_storage_interference/2026_02_25_two_interference_sweep.py
@@ -25,6 +25,7 @@ OUTPUT_DIR = ('/Users/alexkolar/Desktop/Lab/lab-plotting/output_figs/ring_resona
               '/mounted_mk_5/10mK_echo/interference_sweep/no_phase_offset')
 OUTPUT_DIR_PHASE = ('/Users/alexkolar/Desktop/Lab/lab-plotting/output_figs/ring_resonators'
                     '/mounted_mk_5/10mK_echo/interference_sweep/phase_offset')
+SAVE_ALL_FIGS = False
 color_no_phase = 'cornflowerblue'
 color_phase = 'coral'
 
@@ -47,24 +48,26 @@ for file in storage_files:
     pi_fraction = float(file_parts[2]) + (float(file_parts[3][:2])/100)
     all_phases.append(pi_fraction)
 
-    # plot interference
     time = df['time(ps)']
     time /= 1e6  # convert to us
     counts = df['counts']
-    plt.plot(time, counts, color=color_no_phase)
-    plt.axvspan(interference_window[0], interference_window[1],
-                alpha=0.2, color='gray', label='Interference Count Window')
-    plt.title(rf'Comb Separation $\Delta\phi = {pi_fraction}\pi$')
-    plt.ylim(0, 800)
-    plt.xlim(xlim)
-    plt.xlabel(r'Time ($\mu$s)')
-    plt.ylabel(r'Counts')
-    plt.tight_layout()
 
-    file_base = os.path.splitext(file)[0]
-    output_filename = file_base + '.png'
-    plt.savefig(os.path.join(OUTPUT_DIR, output_filename))
-    plt.clf()
+    # plot interference
+    if SAVE_ALL_FIGS:
+        plt.plot(time, counts, color=color_no_phase)
+        plt.axvspan(interference_window[0], interference_window[1],
+                    alpha=0.2, color='gray', label='Interference Count Window')
+        plt.title(rf'Comb Separation $\Delta\phi = {pi_fraction}\pi$')
+        plt.ylim(0, 800)
+        plt.xlim(xlim)
+        plt.xlabel(r'Time ($\mu$s)')
+        plt.ylabel(r'Counts')
+        plt.tight_layout()
+
+        file_base = os.path.splitext(file)[0]
+        output_filename = file_base + '.png'
+        plt.savefig(os.path.join(OUTPUT_DIR, output_filename))
+        plt.clf()
 
     # gather relevant data
     int_idx = np.where(np.logical_and(time > interference_window[0], time < interference_window[1]))[0]
@@ -83,24 +86,26 @@ for file in storage_files_phase:
     pi_fraction = float(file_parts[4]) + (float(file_parts[5][:2])/100)
     all_phases_phase.append(pi_fraction)
 
-    # plot interference
     time = df['time(ps)']
     time /= 1e6  # convert to us
     counts = df['counts']
-    plt.plot(time, counts, color=color_phase)
-    plt.axvspan(interference_window[0], interference_window[1],
-                alpha=0.2, color='gray', label='Interference Count Window')
-    plt.title(rf'Comb Separation $\Delta\phi = {pi_fraction}\pi$')
-    plt.ylim(0, 800)
-    plt.xlim(xlim)
-    plt.xlabel(r'Time ($\mu$s)')
-    plt.ylabel(r'Counts')
-    plt.tight_layout()
 
-    file_base = os.path.splitext(file)[0]
-    output_filename = file_base + '.png'
-    plt.savefig(os.path.join(OUTPUT_DIR_PHASE, output_filename))
-    plt.clf()
+    # plot interference
+    if SAVE_ALL_FIGS:
+        plt.plot(time, counts, color=color_phase)
+        plt.axvspan(interference_window[0], interference_window[1],
+                    alpha=0.2, color='gray', label='Interference Count Window')
+        plt.title(rf'Comb Separation $\Delta\phi = {pi_fraction}\pi$')
+        plt.ylim(0, 800)
+        plt.xlim(xlim)
+        plt.xlabel(r'Time ($\mu$s)')
+        plt.ylabel(r'Counts')
+        plt.tight_layout()
+
+        file_base = os.path.splitext(file)[0]
+        output_filename = file_base + '.png'
+        plt.savefig(os.path.join(OUTPUT_DIR_PHASE, output_filename))
+        plt.clf()
 
     # gather relevant data
     int_idx = np.where(np.logical_and(time > interference_window[0], time < interference_window[1]))[0]
@@ -113,6 +118,7 @@ model = SineModel() + ConstantModel()
 params = model.make_params(amplitude=4500, frequency=np.pi, c=4500)
 params['frequency'].vary = False
 
+# fit for no phase shift state
 res = model.fit(all_interference_counts, params, x=all_phases)
 print(res.fit_report())
 
@@ -127,6 +133,7 @@ print(f'Fidelity: {(visibility+1)/2:.4f} +/- {visibility_err/2:.4f}')
 maximum = (np.pi/2 - res.params['shift']) / res.params['frequency']
 print(f'Calculated Maximum Point: {maximum}')
 
+# fit for phase-shifted state
 res_phase = model.fit(all_interference_counts_phase, params, x=all_phases_phase)
 print(res_phase.fit_report())
 
@@ -143,15 +150,15 @@ print(f'Calculated Maximum Point: {maximum}')
 
 # final plotting of points and fitted fringe
 x_points_for_eval = np.linspace(min(all_phases), max(all_phases), 1000)
-plt.plot(all_phases, all_interference_counts,
-         color=color_no_phase, ls='', marker='o',
-         label=rf'$|e\rangle + |\ell\rangle$')
+plt.errorbar(all_phases, all_interference_counts, yerr=np.sqrt(all_interference_counts),
+             color=color_no_phase, ls='', marker='o', capsize=3,
+             label=rf'$|e\rangle + |\ell\rangle$')
 plt.plot(x_points_for_eval, res.eval(x=x_points_for_eval),
          color=color_no_phase, ls='--',
          label='Fit')
-plt.plot(all_phases_phase, all_interference_counts_phase,
-         color=color_phase, ls='', marker='s',
-         label=rf'$|e\rangle + i|\ell\rangle$')
+plt.errorbar(all_phases_phase, all_interference_counts_phase, yerr=np.sqrt(all_interference_counts_phase),
+             color=color_phase, ls='', marker='s', capsize=3,
+             label=rf'$|e\rangle + i|\ell\rangle$')
 plt.plot(x_points_for_eval, res_phase.eval(x=x_points_for_eval),
          color=color_phase, ls='--',
          label='Fit')
